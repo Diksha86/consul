af0aa0f11a8eda5718468fef2198d0ed6c067918
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-23 11:51:37 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {}
+variable "gcp_project" {}
+variable "credentials" {}
+variable "consulvpc" {}
+variable "consulsub" {}
+// variable "elk_instance_name" {}
+variable "instance_name" {}
+variable "consul_instance_ip_name" {}
+variable "consul_instance_ip_region" {}
