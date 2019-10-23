c26bbd59ffffaad74aaf60ed65a89d4f33896fcf
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-23 11:51:55 +0000

    Update consul.sh

--- consul.sh
+++ consul.sh
@@ -1,1 +1,12 @@
-
+sudo -i
+yum -y install go
+yum -y install git
+mkdir -p $GOPATH/src/github.com/hashicorp
+cd !$
+git clone https://github.com/hashicorp/consul.git
+cd consul
+make tools
+make dev
+cd $GOPATH/src/github.com/hashicorp
+cd consul
+cd bin
